6d655cc8c71ac929c140cebe3d58475319f048ac
刘艳芬 <1341450824@qq.com>
2019-04-24 10:58:14 +0800

    api-auto-test

---
+++ TestCase/test_pytest2.py
@@ -0,0 +1,11 @@
+# 语法糖
+class Test_py:
+    def test_demo1(self):
+        a = 1
+        b = 1
+        assert a==b
+
+    def test_demo2(self):
+        a = 1
+        b = 1
+        assert a == b
